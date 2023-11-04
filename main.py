b416f5ecd33a43a54854d45fb6ec2c7e5135fcef
prachutony <131603023+prachutony@users.noreply.github.com>
2023-11-04 19:37:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 st.set_page_config(
-    page_title="VIT Student Helper",
+    page_title="VIT Student Saviour",
     initial_sidebar_state="expanded"
 )
 
